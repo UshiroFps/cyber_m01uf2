1b992119eb70b184dff05d39349d33d26ebbab0a
Daniel Gago <daniel.gago@enti.cat>
2025-03-29 22:46:18 +0100

    Ejercico Final UF

--- cliente.sh
+++ cliente.sh
@@ -1,3 +1,102 @@
 #!/bin/bash
 
+if [ $# -ne 1  ]; then
 
+	echo "ERROR: Malformed instruction. This command requires at least one parameter."
+
+	echo -e "\n\tUse example: $0 127.0.0.1"
+
+	echo " "
+
+	exit 1
+fi
+
+PORT=7777
+
+IP_SERVER=$1
+
+FILE="saludo.ogg"
+
+IP_CLIENT=`ip a | grep "inet " | grep global | cut -d " " -f 6 | cut -d "/" -f 1`
+
+echo "LSTP Client (Lechuga Speaker Transfer Protocol)"
+
+echo "1. SEND HEADER"
+
+echo "LSTP_1.1 $IP_CLIENT" | nc $IP_SERVER $PORT
+
+echo "2. LISTEN OK_HEADER"
+
+DATA=`nc -l $PORT`
+
+echo "6. CHECK OK_HEADER"
+
+if [ "$DATA" != "OK_HEADER"  ]; then
+
+	echo "ERROR 1: Header not sent correctly. $DATA"
+
+	exit 1
+fi
+
+#text2wave client/lechuga1.lechu -o client/lechuga1.wav
+
+#ffmpeg -i client/lechuga1.wav client/lechuga1.ogg
+
+echo "7.1 SEND NUM_FILES"
+NUM_FILES=`ls *.ogg | wc -l`
+echo "NUM_FILES $NUM_FILES" | nc $IP_SERVER $PORT
+
+DATA=`nc -l $PORT`
+
+echo "7.2 CHECK OK_NUM_FILES"
+if [ "$DATA" != "OK_NUM_FILES" ]; then
+	echo "ERROR 21: NUM_FILES enviado incorrectamente"
+	exit 21
+fi
+
+
+echo "7.3 SEND FILES"
+for FILE in `ls *.ogg`
+do
+
+echo "7.x SEND FILE_NAME"
+echo "FILE_NAME $FILE" | nc $IP_SERVER $PORT
+
+
+echo "8. LISTEN PREFIX_OK"
+
+DATA=`nc -l $PORT`
+
+if [ "$DATA" != "OK_FILE_NAME" ]; then
+	
+	echo "ERROR 2: Filename not set correctly. $DATA"
+
+	exit 2
+
+fi
+
+echo "12. SEND FILE_DATA"
+
+cat "$FILE" | nc $IP_SERVER $PORT
+
+echo "13. LISTEN OK_FILE_DATA"
+
+DATA=`nc -l $PORT`
+
+echo "16. SEND FILE_DATA_MD5"
+
+MD5=`cat $FILE | md5sum | cut -d " " -f 1`
+
+echo "FILE_DATA_MD5 $MD5" | nc  $IP_SERVER $PORT
+
+echo "17. LISTEN FILE_DATA_MD5"
+DATA=`nc -l $PORT`
+
+echo "21. CHECK OK_FILE_MD5"
+if [ "$DATA" != "OK_FILE_MD5" ]; then
+	echo "ERROR 4: MD5 no encaja."
+	exit 4
+fi
+done
+echo "END"
+exit 0
--- server.sh
+++ server.sh
@@ -1,1 +1,140 @@
+#!/bin/bash
 
+PORT=7777
+
+echo "LSTP server (Lechuga Speaker Transfer Protocol)"
+
+echo "0. LISTEN"
+
+DATA=`nc -l $PORT`
+
+PROTOCOL_PREFIX=`echo "$DATA" | cut -d " " -f 1`
+
+echo "3. CHECK HEADER"
+
+IP_CLIENT=`echo "$DATA" | cut -d " " -f 2`
+
+if [ "$PROTOCOL_PREFIX" != "LSTP_1.1"  ]; then
+
+	echo "ERROR 1: Header malformed. $DATA"
+
+	echo "KO_HEADER" | nc $IP_CLIENT $PORT
+
+	exit 1
+fi
+
+echo "4. SEND OK_HEADER"
+
+echo "OK_HEADER" | nc $IP_CLIENT $PORT
+
+echo "5.1 LISTEN NUM_FILES"
+DATA=`nc -l $PORT`
+PREFIX=`echo $DATA | cut -d " " -f 1`
+if [ "$PREFIX" != "NUM_FILES" ]; then
+	echo "ERROR 22: Numero de archivo mal formado (PREFIJO mal formado)"
+	echo "KO_NUM_FILE" | nc $IP_CLIENT $PORT
+	exit 22
+fi
+
+NUM_FILES=`echo $DATA | cut -d " " -f 2`
+
+NUM_FILES_CHECK=`echo $NUM_FILES | grep -E "^-?[0-9]+$"`
+if [ "$NUM_FILES_CHECK" == "" ]; then
+	echo "ERROR 22 : Numero de archivos incorrecto (No es un numero)"
+	echo "KO_NUM_FILES" | nc $IP_CLIENT $PORT
+	exit 22
+fi
+
+if [ "$NUM_FILES" -lt 1 ]; then
+	echo "ERROR 22: Numero de archivo mal formado (Numero inferior a 1)"
+	echo "KO_NUM_FILES" | nc $IP_CLIENT $PORT
+	exit 22
+fi
+
+echo "OK_NUM_FILES" | nc $IP_CLIENT $PORT
+
+for NUM in `seq $NUM_FILES`
+
+do
+
+
+echo "5.x LISTEN FILE_NAME"
+
+DATA=`nc -l $PORT`
+
+FILE_NAME_PREFIX=`echo "$DATA" | cut -d " " -f 1`
+
+echo "9. CHECK FILE_NAME"
+
+if [ "$FILE_NAME_PREFIX" != "FILE_NAME"  ]; then
+	
+	echo "ERROR 2: Prefix unkown. $DATA"
+
+	echo "KO_FILE_NAME"
+	
+	exit 2
+
+fi
+
+FILE_NAME=`echo "$DATA" | cut -d " " -f 2`
+echo $FILE_NAME
+
+echo "10. SEND OK_FILE_NAME"
+
+echo "OK_FILE_NAME" | nc $IP_CLIENT $PORT
+
+echo "11. LISTEN FILE_DATA"
+
+nc -l $PORT > $FILE_NAME
+
+echo "14. SEND OK_FILE_DATA"
+
+FILE_SIZE=`ls -l $FILE_NAME | cut -d " " -f 5`
+
+if [ $FILE_SIZE -eq 0 ]; then
+
+	echo "ERROR 3: No file data. File size: $FILE_SIZE B."
+
+	echo "KO_FILE_DATA"
+
+	exit 3
+
+fi
+
+echo "OK_FILE_DATA" | nc $IP_CLIENT $PORT
+
+echo "15. LISTEN MD5"
+
+DATA=`nc -l $PORT`
+echo $DATA
+
+echo "18. COMPROBACION PREFIJO MD5 RECIBIDO"
+
+PREFIX_MD5=`echo $DATA | cut -d " " -f 1`
+
+if [ "$PREFIX_MD5" != "FILE_DATA_MD5" ]
+then
+    echo "ERROR 5: Prefijo MD5 incorrecto"
+    echo "KO_FILE_DATA_MD5" | nc $IP_CLIENT $PORT
+    exit 5
+fi
+
+echo "19. CHECK MD5"
+
+MD5_COMPROBAR=`cat "$FILE_NAME" | md5sum | cut -d " " -f 1`
+HASH_MD5=`echo $DATA | cut -d " " -f 2`
+
+if [ "$HASH_MD5" != "$MD5_COMPROBAR" ]
+then
+    echo "ERROR 6: El MD5 no encaja."
+    echo "KO_FILE_DATA_MD5" | nc $IP_CLIENT $PORT
+    exit 6
+fi
+echo "20. SEND OK_MD5"
+
+echo "OK_FILE_DATA_MD5" | nc $IP_CLIENT $PORT
+
+done
+
+echo "END"
+exit 0
